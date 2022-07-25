2593518c11343f32e58cc177cbc40fd7d3b91589
Terry Grimoire <terry.grimoire@gmail.com>
2022-07-25 22:38:06 +0200

    menu burger is now functional with animation as well and navigation ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import burger from "../assets/menu.png";
+import croix from "../assets/croix.png";
 import logo from "../assets/Montecito.gif";
 
 function Burger() {
+  const [openBurger, setOpenBurger] = useState(false);
+
   return (
     <header className="dark flex between padding-header">
-      <button type="button">
+      <button type="button" onClick={() => setOpenBurger(!openBurger)}>
         <img
           src={burger}
-          alt="navigation menu for mobile - three lines navigation"
+          alt="menu de navigation sur le site internet - trois lignes de navigation"
         />
       </button>
       <img src={logo} alt="logo of Montecito" className="logo-mobile" />
+      {openBurger && (
+        <nav className="flex col openBurger between">
+          <button
+            type="button"
+            onClick={() => setOpenBurger(false)}
+            className="croix"
+          >
+            <img
+              src={croix}
+              alt="croix pour fermer la navigation sur la page"
+            />
+          </button>
+          <Link to="/">
+            <button type="button" onClick={() => setOpenBurger(false)}>
+              Accueil
+            </button>
+          </Link>
+          <Link to="/Carte">
+            <button type="button" onClick={() => setOpenBurger(false)}>
+              Carte
+            </button>
+          </Link>
+          <Link to="/Evenements">
+            <button type="button" onClick={() => setOpenBurger(false)}>
+              Evenements
+            </button>
+          </Link>
+          <Link to="/Contact">
+            <button type="button" onClick={() => setOpenBurger(false)}>
+              Contact
+            </button>
+          </Link>
+        </nav>
+      )}
     </header>
   );
 }
